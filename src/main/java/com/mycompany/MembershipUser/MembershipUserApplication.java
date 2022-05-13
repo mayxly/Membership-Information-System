4e08bd83fcb81bfdb93f50a82f6b3e629f266a92
mayxly <may.ly7303@gmail.com>
2022-05-12 22:24:13 -0400

    Initial Commit.

---
+++ src/main/java/com/mycompany/MembershipUser/MembershipUserApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.MembershipUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/MembershipUser/MembershipUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.MembershipUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
