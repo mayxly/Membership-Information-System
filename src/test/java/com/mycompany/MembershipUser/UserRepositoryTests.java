120dbb5ad059b6c22ebf4b168d4aa1ff0cb0a7dd
mayxly <may.ly7303@gmail.com>
2022-05-13 01:29:47 -0400

    Create test file for adding new user.

--- src/test/java/com/mycompany/MembershipUser/UserRepositoryTests.java
+++ src/test/java/com/mycompany/MembershipUser/UserRepositoryTests.java
@@ -1,4 +1,31 @@
 package com.mycompany.MembershipUser;
 
+import com.mycompany.MembershipUser.user.User;
+import com.mycompany.MembershipUser.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
 public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew() {
+        User user = new User();
+        user.setEmail("graceli@gmail.com");
+        user.setPassword("password1234");
+        user.setFirstName("Grace");
+        user.setLastName("Li");
+
+        User savedUser = repo.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
 }
