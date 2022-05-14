7fceaf358af11a158c8f6e6dfa26ab38083f3ce5
mayxly <may.ly7303@gmail.com>
2022-05-13 23:11:15 -0400

    Code save and cancel button.

--- src/main/java/com/mycompany/MembershipUser/user/UserController.java
+++ src/main/java/com/mycompany/MembershipUser/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -21,5 +22,11 @@ public class UserController {
     public String showNewForm(Model model) {
         model.addAttribute("user", new User());
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user) {
+        service.save(user);
+        return "redirection:/users";
     }
 }
