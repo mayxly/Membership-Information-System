89e519bdac4756a9290b8d5d73e3ac118108698d
mayxly <may.ly7303@gmail.com>
2022-05-14 10:51:23 -0400

    Create saved successfully message.

--- src/main/java/com/mycompany/MembershipUser/user/UserController.java
+++ src/main/java/com/mycompany/MembershipUser/user/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class UserController {
@@ -25,8 +27,9 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user) {
+    public String saveUser(User user, RedirectAttributes ra) {
         service.save(user);
-        return "redirection:/users";
+        ra.addFlashAttribute("message", "The new member has been saved successfully.");
+        return "redirect:/users";
     }
 }
